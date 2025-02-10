241368e3e876bc0cb7abc78636c98375c5e100fb
jitendraparmar10 <jitparmar993@gmail.com>
2025-02-11 00:52:54 +0530

    Added session management and CORS configuration

--- src/index.js
+++ src/index.js
@@ -9,9 +9,23 @@ dotenv.config();
 const app = express();
 
 //Middlewares
+const corsOptions = {
+    origin: ["http:/localhost:3001"],
+    credentials: true,
+};
+app.use(cors(corsOptions));
 app.use(express.json({ limit:"100mb"}));
 app.use(express.urlencoded({limit: "100mb" , extended: true}));
-
+app.use(session({
+    secret:process.env.SESSION_SECRET|| "secret",
+    resave:false,
+    saveUninitialized:false,
+    cookie:{
+        maxAge:6000 * 60,
+    },
+}));
+app.use(passport.initialize());
+app.use(passport.session());
 
 //Routes
 
